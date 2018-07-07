eabae518407c7689e9015eabaca5fd1528106ddc
YuzuHamada <citron2629@icloud.com>
2018-07-07 13:18:46 +0900

    ホワイトボックスというか試作品です。一応あげておきます。

---
+++ gg/Assets/gg_test/LaserPointer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool IsActive = true;
+    public float defaultLength = 0.5f;
+
+    public bool shotRay = true;
+    public float rayLength = 500f;
+    public LayerMask rayExclusionLayers;
+
+    public Transform target;
+    public Transform anchor;
+    public LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Awake () {
+        if (lineRenderer == null)
+            lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!IsActive)
+        {
+            lineRenderer.enabled = false;
+            return;
+        }
+
+        if (shotRay)
+        {
+            Ray ray = new Ray(anchor.position, anchor.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, rayLength, -rayExclusionLayers))
+            {
+                target = hit.transform;
+                DrawTo(hit.point);
+                return;
+            }
+
+            target = null;
+        }
+
+        if (target != null)
+            DrawTo(target.position);
+        else
+            DrawTo(anchor.position + anchor.forward * defaultLength);
+	}
+
+    void DrawTo(Vector3 pos)
+    {
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, anchor.position);
+        lineRenderer.SetPosition(1, pos);
+    }
+}
---
+++ gg/Assets/gg_test/player_move.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+
+    private Vector3 velocity;
+    private Vector3 velocity2;
+    private Vector3 velocity3;
+    private Vector3 velocity4;
+    private Vector3 velocity5;
+
+
+    public GameObject Rock;
+    private float chargeCount = 0;
+    
+    // Use this for initialization
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector3(0, 0, 20);
+        velocity2 = new Vector3(0, 10, 0);
+        velocity3 = new Vector3(0, -10, 0);
+        velocity4 = new Vector3(10, 0, 0);
+        velocity5 = new Vector3(-10, 0, 0);
+
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+
+        if (Input.GetKey("up"))
+        {
+            transform.localPosition += velocity2 * Time.fixedDeltaTime;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.localPosition += velocity3 * Time.fixedDeltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.localPosition += velocity4 * Time.fixedDeltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.localPosition += velocity5 * Time.fixedDeltaTime;
+        }
+
+        
+
+
+        }
+
+    
+}
+
