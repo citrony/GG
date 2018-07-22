4ab19de186e6b3f32fe7db8b681766a2627a0938
YuzuHamada <citron2629@icloud.com>
2018-07-22 22:22:41 +0900

    コピーしたデータです、合わせる前に動作確認お願いします！by濵田

---
+++ gg/Assets/gg_test/Scripts/Test2/MoveLaser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLaser : MonoBehaviour {
+
+    [SerializeField] private int speed = 2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += this.transform.forward * Time.deltaTime * speed;
+	}
+}
---
+++ gg_copy/Assets/gg_test/fove_test/FoveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoveController : MonoBehaviour
+    {
+        public FoveInterfaceBase foveInterface;
+        public GameObject myCube;
+
+        // Update is called once per frame
+        void Update()
+        {
+            myCube = GameObject.Find("Cube");
+
+            Fove.Managed.EFVR_Eye state = FoveInterface.CheckEyesClosed();
+
+            switch (state)
+            {
+                case Fove.Managed.EFVR_Eye.Left:
+                myCube.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            }
+            
+           
+
+        }
+    }
+
